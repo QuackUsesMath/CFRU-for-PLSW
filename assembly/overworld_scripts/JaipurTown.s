f9948d09a61249ec81e8c4fe5cbb9fc21ef4f789
Ansh00000000 <93500134+Ansh00000000@users.noreply.github.com>
2022-12-03 10:16:15 +0530

    update JaipurTown.s

--- assembly/overworld_scripts/JaipurTown.s
+++ assembly/overworld_scripts/JaipurTown.s
@@ -1,1 +1,24 @@
+.text
+.thumb
+.align 2
 
+.include "../xse_commands.s"
+.include "../xse_defines.s"
+.include "../asm_defines.s"
+
+
+
+.global NpcScriptFiighter
+NpcScriptFighter:
+    msgbox MSGNpcFighter MSG_FACE   @;Fighting is greater than...
+    end
+
+
+.global NpcScriptFatDude
+NpcScriptFatDude:
+    msgbox MSGNpcFatDude MSG_FACE   @;Do you know about the Multiverse?...
+    end
+
+
+
+    
